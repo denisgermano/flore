7d2019681dfc7ddbdfa4bb23abfbe7196d6f84e8
Marcus Pereira <mpereirassa@gmail.com>
2020-10-21 09:07:12 -0300

    update

--- flore/table.py
+++ flore/table.py
@@ -4,12 +4,10 @@ from .normalize import normalize
 def compose(table_name, columns):
     tables = ''
     for name, fields in columns.items():
-        text =  normalize(fields)
-        tables += "%s %s, " %(name, text)
-
-    return mount(table_name, tables)
+        tables += "%s %s, " %(name, normalize(fields))
+    return mount_table(table_name, tables)
 
 
-def mount(table_name, tables):
+def mount_table(table_name, tables):
     table = "CREATE TABLE if not exists {0} ({1});".format(table_name, tables)
     return table.replace(", )", ")")
